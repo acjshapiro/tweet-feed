f715827c81d6496bdf285ace4f31f26cf9ec63a4
Alex Shapiro <acjshapiro@gmail.com>
2018-03-20 11:52:53 -0600

    search

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-css';
+import { Grid, Header, Input } from 'semantic-ui-react';
 import axios from 'axios';
 import Tweets from './Tweets';
 
 class App extends Component {
-  state = { tweets: [] }
+  state = { tweets: [], visible: [], search: '' }
 
   componentDidMount() {
     axios.get('/api/tweets')
-    .then( res => this.setState({ tweets: res.data }) )
+      .then( res => this.setState({ 
+        tweets: res.data, visible: res.data 
+      }) 
+    )
   }
 
+  handleChange = (e) => {
+    this.setState({ search: e.target.value}, () => {
+      this.updateVisible()
+    })
+  }
+
+  updateVisible = () => {
+    const { search, tweets } = this.state;
+    if (search.length === 0)
+      this.setState({ visible: tweets })
+    else if (search.length > 3) {
+      axios.get(`/api/search?term=${search}`)
+        .then( res => this.setState({ visible: res.data }) )
+    }
+  }
   render() {
     return (
       <Grid>
@@ -20,14 +38,20 @@ class App extends Component {
           tablet={16}
           computer={4}
           >
-
+            <Header as="h2" textAlign="center">Search</Header>
+            <Input
+              value={this.state.search}
+              onChange={this.handleChange}
+              icon={{ name: 'search', circular: true }}
+              placeholder="Search..."
+              />
           </Grid.Column>
           <Grid.Column
             mobile={16}
             tablet={16}
             computer={12}
             >
-            <Tweets tweets={this.state.tweets} />
+            <Tweets tweets={this.state.visible} />
           </Grid.Column>  
         </Grid.Row>  
       </Grid>
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get 'tweets', to: 'tweets#index'
+    get 'search', to: 'tweets#search'
   end
 
   #Do not place any routes below this one
